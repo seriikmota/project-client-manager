82282b2071b318cdc80ee772020c5bb7aa0d749f
Erik Mota <seriikmota.dev@gmail.com>
2024-12-12 14:08:56 -0300

    Initial commit

---
+++ src/main/java/dev/erikmota/clientmanager/ClientManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.erikmota.clientmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagerApplication.class, args);
+	}
+
+}
